32e984a6ef576c009841ab85374eae1050358a28
rainstains <biladt.hakim@gmail.com>
2022-11-14 13:36:50 +0700

    Login - Positive

--- tests/quiz-16.spec.js
+++ tests/quiz-16.spec.js
@@ -4,6 +4,7 @@ test('Login - Negative Test Case', async ({ page }) => {
 
   await page.goto('https://www.saucedemo.com/');
 
+  //fill invalid username & password
   await page.locator('[data-test="username"]').fill('premium_user');
   await page.locator('[data-test="password"]').fill('open_sauce');
 
@@ -15,6 +16,25 @@ test('Login - Negative Test Case', async ({ page }) => {
 
   await expect(errorDiv).toContainText(/Epic sadface/);
 
-  page.close();
+  await page.close();
+
+});
+
+
+test('Login - Positive Test Case', async ({ page }) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+  //fill valid username & password
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect('[id="logout_sidebar_link"]').toBeDefined();
+
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+  await page.close();
 
 });
